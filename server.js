91b9b203cd0c838ce5cc21180e1794b65e763b99
phatsara <phatsara_ae@hotmail.com>
2018-09-17 10:01:01 +0700

    Create server.js

--- server.js
+++ server.js
@@ -6,5 +6,25 @@ app.get('/home', function (req, res) {
     res.render('pages/home');
 });
 
+app.get('/students', function (req, res) {
+    res.render('pages/home');
+
+    connection.query('SELECT * from students', function (err, rows, fields) {
+        if (err) throw err
+      
+        console.log('The solution is: ', rows[0].solution)
+    })
+});
+
+app.get('/subjects', function (req, res) {
+    res.render('pages/home');
+
+    connection.query('SELECT * from subjects', function (err, rows, fields) {
+        if (err) throw err
+      
+        console.log('The solution is: ', rows[0].solution)
+    })
+});
+
 console.log('App is running at http://localhost:8081');
 app.listen(8081);
