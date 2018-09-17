c59721ce934169a2dca800c4a8c1af09007f826a
phatsara <phatsara_ae@hotmail.com>
2018-09-17 09:50:18 +0700

    Create server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var mysql = require('express')
+var express = require('express')
 var app = express();
 app.set('view engine', 'ejs');
 
@@ -6,5 +6,5 @@ app.get('/home', function (req, res) {
     res.render('pages/home');
 });
 
-console.log('App is running at http://localhost:8080');
-app.listen(8080);
+console.log('App is running at http://localhost:8081');
+app.listen(8081);
